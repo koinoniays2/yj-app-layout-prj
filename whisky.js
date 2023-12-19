d2995705c8edd1b64ff9cd51dad639f900782715
koinoniays <koinonia_ys@naver.com>
2023-12-20 02:21:30 +0900

    app-layout-1

--- whisky.js
+++ whisky.js
@@ -1,4 +1,5 @@
 gsap.registerPlugin(ScrollTrigger);
+/* 이미지 배경 올라오게 하기 */
 gsap.to('#img-container .back-img', {
     scrollTrigger: {
         trigger: '#main-container',
@@ -10,6 +11,7 @@ gsap.to('#img-container .back-img', {
     opacity: 0
 });
 
+/* Shall we whisky? 글씨 애니메이션 */
 let tl = gsap.timeline();
 gsap.fromTo('#main-content span:nth-child(1)',
     { x: -400 },
@@ -24,38 +26,39 @@ tl.fromTo('#main-content span:nth-child(2)',
         opacity: 1
     });
 
+/* 헤더 밑 텍스트 바 */
 gsap.to('.content-text', {
     scrollTrigger: {
         trigger: '#main-container',
         start: 'bottom-=300px top+=100px',
-        scrub: 1,
+        scrub: true,
         end: '90% top+=200px',
         // markers: true
     },
-    display: 'block',
-    opacity: 1
+    opacity: 1, display: "block"
 });
 
+/* 텍스트바 내용 바뀜 */
 const img = document.querySelectorAll('img');
 const contentText = document.querySelector(".content-text");
 
 //contentText.getBoundingClientRect().bottom //109.796875
 window.addEventListener("scroll", function(){
     img.forEach((item) => {
-        if(item.getBoundingClientRect().top <= 109) {
+        if(item.getBoundingClientRect().top <= 110) {
             contentText.innerText = item.alt;
         }
     });
 });
 
+/* 이미지 스크롤 */
 img.forEach((item, i) => {
-    tl.fromTo(item, { x:10 * (i + 1) },
+    tl.fromTo(item, { x:20 * (i + 1) },
         { scrollTrigger: {
         trigger: item,
         start: `top center`,
         scrub: 1,
-        end: `top center-=100px`,
-        // markers: true
+        end: `bottom center-=100px`,
         }, x: 0 
     });
 });
